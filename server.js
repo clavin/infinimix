17422f1004a4a350bea47944e1a405f0b36631e7
clavin <clavin@users.noreply.github.com>
2017-10-08 02:41:29 -0600

    :construction: :fire: Prepare to move audio processing to client

--- server.js
+++ server.js
@@ -1,52 +1,38 @@
 const express = require('express');
-const expressFileUpload = require('express-fileupload');
+const ytdl = require('ytdl-core');
 
-const getPCMData = require('./lib/getPCMData.js');
-const getYouTubeSong = require('./lib/getYouTubeSong.js');
+/** The maximum allowed length of a video. */
+const MaxYouTubeLength = 60 * 15;
 
-const port = 8080;
+/** The port to run the express `app` on. */
+const Port = process.env.PORT || 8080;
 const app = express();
 
 // Publish everything in the `public/` directory.
 app.use(express.static('./public'));
 
-// Handle file uploads using `express-fileupload`.
-app.use(expressFileUpload());
+// Proxy the song with the given id to the client (same-origin policy restricts this on the client).
+app.get('/api/yt/song/:id', (req, res) => {
+  ytdl.getInfo(req.params.id)
+    .then((info) => {
+      if (info.length_seconds > MaxYouTubeLength)
+        return Promise.reject(new Error(`Length of video exceeds ${MaxYouTubeLength} seconds; too long.`));
 
-/**
- * A small utility to respond with raw PCM.
- * @param {Response} res - The express.js response object for the request.
- * @param {number[]} pcm - The PCM to respond with.
- * @returns {void}
- */
-function respondPCM(res, pcm) {
-  res.set('Content-Type', 'application/octet-stream');
-  res.send(pcm);
-}
+      const format = ytdl.chooseFormat(info.formats, { filter: 'audioonly' });
 
-app.get('/api/pcm/yt/:id', (req, res) => {
-  // Get the song, turn it into raw PCM, and send it back to the client.
-  getYouTubeSong(req.params.id)
-    .then((song) => getPCMData(song.stream))
-    .then((pcm) => respondPCM(res, pcm))
-    .catch((err) => res.status(500).send(err));
-});
+      // Set the status to 200 and pipe the song back.
+      if (format.type)
+        res.type(format.type);
 
-app.get('/api/pcm/upload', (req, res) => {
-  // Make sure there's a song file that's been uploaded.
-  if (!req.files || !req.files.song)
-    return res.status(400).send('Expecting upload.');
-  
-  // Get the PCM data from the song and send it back.
-  getPCMData(req.files.song.data)
-    .then((pcm) => respondPCM(res, pcm))
-    .catch((err) => res.status(500).send(err));
+      res.status(200);
+      ytdl.downloadFromInfo(info, { format: format }).pipe(res);
+    })
+    .catch((err) => res.status(500).send(err.toString()).end());
 });
 
 // And we're off to the internets!
-app.listen(port, (err) => {
+app.listen(Port, (err) => {
   if (err)
     return console.log(err);
-
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${Port}`);
 });
